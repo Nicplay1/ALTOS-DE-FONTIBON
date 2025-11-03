c9feee8f18a65fa6cc860ba342e3807623707f86
Nicplay1 <nicolasballesteros900@gmail.com>
2025-11-03 10:02:27 -0500

    correcion 5

--- app/build.sh
+++ app/build.sh
@@ -1,12 +1,17 @@
-#!/bin/bash
-set -e
+#!/usr/bin/env bash
+set -o errexit
 
 echo "🚀 Instalando dependencias..."
 pip install -r requirements.txt
 
-echo "⚙️ Aplicando migraciones..."
-python manage.py makemigrations usuario vigilante residente administrador
-python manage.py showmigrations usuario
+echo "⚙️ Limpiando migraciones viejas..."
+python manage.py migrate usuario zero --noinput
+python manage.py migrate vigilante zero --noinput
+python manage.py migrate residente zero --noinput
+python manage.py migrate administrador zero --noinput
+
+echo "🧩 Volviendo a crear migraciones..."
+python manage.py makemigrations
 python manage.py migrate --noinput
 
 echo "✅ Migraciones aplicadas correctamente."
