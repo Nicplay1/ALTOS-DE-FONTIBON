41b1c865c6186f705fa62dbb232bdf86b93d916c
Nicplay1 <nicolasballesteros900@gmail.com>
2025-11-02 20:36:50 -0500

    forzar migracion

--- app/builds.sh
+++ app/builds.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
-
-# 🔹 Salir si hay un error
 set -e
 
 echo "Instalando dependencias..."
 pip install -r requirements.txt
 
 echo "Ejecutando migraciones..."
-python manage.py migrate
+python manage.py migrate --noinput
+
+echo "Recolectando archivos estáticos..."
+python manage.py collectstatic --noinput
 
 echo "🔥 Build completado."
