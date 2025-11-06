92238a70cf47440a3327a6029338545e3896a1b9
Nicplay1 <nicolasballesteros900@gmail.com>
2025-11-06 12:02:14 -0500

    correciones

--- app/app/asgi.py
+++ app/app/asgi.py
@@ -4,8 +4,7 @@ from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 import residente.routing as residente_routing
 import usuario.routing as usuario_routing
-import vigilante.routing as vigilante_routing
-import administrador.routing as administrador_routing
+
 from .routing import application
 
 
@@ -17,9 +16,7 @@ application = ProtocolTypeRouter({
     "websocket": AuthMiddlewareStack(
         URLRouter(
             residente_routing.websocket_urlpatterns +
-            usuario_routing.websocket_urlpatterns +
-            vigilante_routing.websocket_urlpatterns +
-            administrador_routing.websocket_urlpatterns
+            usuario_routing.websocket_urlpatterns 
         )
     ),
 })
