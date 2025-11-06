7fdaa729802c564a99cb50aaf29c0c61577f9521
Nicplay1 <nicolasballesteros900@gmail.com>
2025-11-06 11:51:50 -0500

    Correcuines

--- app/usuario/consumers.py
+++ app/usuario/consumers.py
@@ -1,7 +1,7 @@
 import json
 from channels.generic.websocket import AsyncWebsocketConsumer
 from django.template.loader import render_to_string
-from .models import *
+
 
 class UsuarioConsumer(AsyncWebsocketConsumer):
     async def connect(self):
@@ -12,6 +12,10 @@ class UsuarioConsumer(AsyncWebsocketConsumer):
         await self.channel_layer.group_discard("usuarios_group", self.channel_name)
 
     async def usuario_actualizado(self, event):
+        # 🔹 Importar los modelos aquí dentro
+        from usuario.models import Usuario, Rol
+          # o desde donde esté el modelo Rol
+
         html = render_to_string("administrador/usuario/tabla_usuarios.html", {
             "usuarios": Usuario.objects.select_related("id_rol").all(),
             "roles": Rol.objects.all(),
